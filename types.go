83f112886ef418e9253b53b374e422e88223844e
edwardowens <eddiebeme@gmail.com>
2024-12-16 11:57:52 -0800

    feat: Adding a map type for config.

--- types.go
+++ types.go
@@ -12,7 +12,7 @@ func NewList(ss ...string) List {
 
 // ToSlice convert the list to a slice of strings.
 func (l List) ToSlice() []string {
-	ss := strings.Split(string(l), ",")
+	ss := Split(string(l))
 	out := make([]string, 0, len(ss))
 	for _, v := range ss {
 		if v == "" {
@@ -25,4 +25,53 @@ func (l List) ToSlice() []string {
 
 func (l List) String() string {
 	return string(l)
+}
+
+// Map alias type to support comma-separated key=value pairs
+type Map string
+
+func NewMap(m map[string]string) Map {
+	strs := make([]string, 0, len(m))
+	for k, v := range m {
+		strs = append(strs, k+"="+v)
+	}
+	return Map(strings.Join(strs, ","))
+}
+
+func (m Map) ToMap() map[string]string {
+	out := map[string]string{}
+	pairs := Split(string(m))
+	m.NavPairs(pairs, func(key, val string) {
+		out[key] = val
+	})
+	return out
+}
+
+func (m Map) ToEnv() []string {
+	pairs := Split(string(m))
+	out := make([]string, 0, len(pairs))
+	m.NavPairs(pairs, func(key, val string) {
+		out = append(out, key+"="+val)
+	})
+	return out
+}
+
+func (m Map) NavPairs(pairs []string, f func(key, val string)) {
+	for _, v := range pairs {
+		kv := strings.Split(v, "=")
+		if len(kv) != 2 {
+			continue
+		}
+		f(kv[0], kv[1])
+	}
+}
+
+func (m Map) String() string {
+	return string(m)
+}
+
+var Splitter = ","
+
+func Split(s string) []string {
+	return strings.Split(s, Splitter)
 }
